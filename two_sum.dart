8b61a0f78a3711a055023820c68862f03371e572
samitad2024 <samigo2024@gmail.com>
2025-05-28 18:03:02 +0300

    two number add with target

---
+++ two_sum.dart
@@ -0,0 +1,21 @@
+List<int> twoSum(List<int> nums, int target) {
+  Map<int, int> numToIndex = {};
+
+  for (int i = 0; i < nums.length; i++) {
+    int complement = target - nums[i];
+    if (numToIndex.containsKey(complement)) {
+      return [numToIndex[complement]!, i];
+    }
+    numToIndex[nums[i]] = i;
+  }
+
+  return [];
+}
+
+void main() {
+  List<int> nums = [2, 7, 11, 15];
+  int target = 9;
+
+  List<int> result = twoSum(nums, target);
+  print(result); // Output: [0, 1]
+}
